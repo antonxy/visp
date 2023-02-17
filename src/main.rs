6b69a77a3c0cab48825fbfddcda54cbcfe9e7ce5
Anton Schirg <anton.schirg@posteo.de>
2023-02-17 16:17:47 -0500

    fix overflows, selectable empty cells

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 // VISP: VI-style SPreadsheet
 
-use std::{io, thread, time::Duration};
+use std::{io, time::Duration};
 use tui::{
     backend::Backend,
     backend::CrosstermBackend,
-    widgets::{Widget, Block, Borders, Paragraph},
+    widgets::{Widget, Paragraph},
     layout::{Layout, Constraint, Direction, Rect},
     buffer::{Buffer},
-    style::{Style, Modifier},
+    style::{Style, Modifier, Color},
     Frame,
     Terminal
 };
 use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    event::{DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
@@ -29,7 +29,7 @@ fn main() -> Result<(), io::Error> {
         cells: vec![
             vec![TableCell::String("Value".to_string()), TableCell::Value(10), TableCell::Value(10)],
             vec![TableCell::String("Value".to_string()), TableCell::Value(20), TableCell::Value(10)],
-            vec![TableCell::String("Value".to_string()), TableCell::Value(20), TableCell::Value(10)],
+            vec![TableCell::String("Value".to_string()), TableCell::Empty, TableCell::Value(10)],
             vec![TableCell::String("Value".to_string()), TableCell::Value(20), TableCell::Value(10)],
         ],
         col_widths: vec![10, 5],
@@ -51,19 +51,22 @@ fn main() -> Result<(), io::Error> {
             let event = crossterm::event::read()?;
 
             if event == Event::Key(KeyCode::Char('j').into()) {
-                content.selection.row += 1;
+                content.selection.row = content.selection.row.saturating_add(1);
             }
             if event == Event::Key(KeyCode::Char('k').into()) {
-                content.selection.row -= 1;
+                content.selection.row = content.selection.row.saturating_sub(1);
             }
             if event == Event::Key(KeyCode::Char('l').into()) {
-                content.selection.col += 1;
+                content.selection.col = content.selection.col.saturating_add(1);
             }
             if event == Event::Key(KeyCode::Char('h').into()) {
-                content.selection.col -= 1;
+                content.selection.col = content.selection.col.saturating_sub(1);
             }
 
             if event == Event::Key(KeyCode::Esc.into()) {
+                break;
+            }
+            if event == Event::Key(KeyCode::Char('q').into()) {
                 break;
             }
         }
@@ -82,6 +85,7 @@ fn main() -> Result<(), io::Error> {
 }
 
 enum TableCell {
+    Empty,
     String(String),
     Value(i32),
 }
@@ -89,6 +93,7 @@ enum TableCell {
 impl TableCell {
     fn format_string(&self) -> String {
         match self {
+            Self::Empty => "".to_string(),
             Self::String(s) => s.clone(),
             Self::Value(v) => format!("{}", v),
         }
@@ -125,44 +130,57 @@ impl<'a> Widget for Table<'a> {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let header_style = Style::default().add_modifier(Modifier::BOLD);
         let column_style = Style::default();
-        let selected_column_style = Style::default().add_modifier(Modifier::BOLD);
+        let selected_column_style = Style::default().fg(Color::White).bg(Color::Black);
 
-        let mut row = 0; //Table row col
+        let draw_cell = |buf: &mut Buffer, cell: Option<&TableCell>, rect: Rect, selected: bool| {
+            let style = if selected {
+                selected_column_style
+            } else {
+                column_style
+            };
+            for x in rect.x..rect.x + rect.width {
+                for y in rect.y..rect.y + rect.height {
+                    buf.get_mut(x, y).set_char(' ').set_style(style);
+                }
+            }
+            if let Some(c) = cell {
+                buf.set_stringn(rect.x, rect.y, c.format_string(), rect.width as usize, style);
+            }
+        };
+
+        let mut row = 0; 
         let mut y = area.y; //Buffer position
 
         while y < area.y + area.height {
-            let mut row_height : u16 = self.content.row_heights.get(row - 1).map(|h| *h).unwrap_or(1);
+            let table_row = if row == 0 { None } else { Some(row - 1) };
+            let row_height : u16 = table_row.and_then(|r| self.content.row_heights.get(r)).map(|h| *h).unwrap_or(1);
+
             let mut col = 0;
             let mut x = area.x;
             while x < area.x + area.width {
-                let mut col_width : u16 = self.content.col_widths.get(col - 1).map(|w| *w).unwrap_or(4);
-                if row == 0 {
+                let table_col = if col == 0 { None } else { Some(col - 1) };
+                let col_width : u16 = table_col.and_then(|c| self.content.col_widths.get(c)).map(|c| *c).unwrap_or(4);
+
+                if let Some(table_row) = table_row {
+                    if let Some(table_col) = table_col {
+                        // Table content
+                        let cell : Option<&TableCell> = self.content.cells.get(table_row).and_then(|r| r.get(table_col));
+                        let selected = self.content.selection.selected(table_row as u16, table_col as u16);
+                        draw_cell(buf, cell, Rect::new(x, y, col_width, row_height).intersection(area), selected);
+                    } else {
+                        // Header column
+                        buf.set_string(x, y, format!("{}", row), header_style);
+                    }
+
+                } else {
                     // Header row
                     if col == 0 {
                         buf.set_string(x, y, "**", header_style);
                     } else {
                         buf.set_string(x, y, format!("{}", col), header_style);
                     }
-                } else {
-                    if col == 0 {
-                        // Header column
-                        buf.set_string(x, y, format!("{}", row), header_style);
-                    } else {
-                        let table_row : u16 = (row - 1).try_into().unwrap();
-                        let table_col : u16 = (col - 1).try_into().unwrap();
-                        // Table content
-                        let content : Option<&TableCell> = self.content.cells.get(table_row as usize).and_then(|r| r.get(table_col as usize));
-                        if let Some(c) = content {
-                            //TODO overflow
-                            let style = if self.content.selection.selected(table_row, table_col) {
-                                selected_column_style
-                            } else {
-                                column_style
-                            };
-                            buf.set_string(x, y, c.format_string(), style);
-                        }
-                    }
                 }
+
                 x += col_width;
                 col += 1;
             }
